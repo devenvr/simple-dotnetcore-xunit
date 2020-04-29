002793068cd70f99a412a92a8661a0f47da784bc
annamalai2k11 <51172131+annamalai2k11@users.noreply.github.com>
2020-04-30 00:23:44 +0530

    Pass unit test

--- dotnetcore-samplewebapitests/WeatherForecastControllerTests.cs
+++ dotnetcore-samplewebapitests/WeatherForecastControllerTests.cs
@@ -13,7 +13,7 @@ namespace Tests
         public void Get_NoArguments_ReturnsDefaultForecastDays()
         {
             var controller = new WeatherForecastController(loggerStub.Object);
-            var expectedDays = 7;
+            var expectedDays = 5;
 
             var forecasts = controller.Get();
 
